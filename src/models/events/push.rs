cde4577632dbf9886d05cbaa0ca40028cfaf6867
Tudor Nica <tudor.nica@visma.com>
2025-06-10 10:06:47 +0300

    fix: remove duplicated structs after upstream update

--- src/models/events/push.rs
+++ src/models/events/push.rs
@@ -54,7 +54,6 @@ pub enum SlackEventCallbackBody {
     AppHomeOpened(SlackAppHomeOpenedEvent),
     AppMention(SlackAppMentionEvent),
     AppUninstalled(SlackAppUninstalledEvent),
-    AssistantThreadStarted(SlackAssistantThreadStartedEvent),
     LinkShared(SlackLinkSharedEvent),
     EmojiChanged(SlackEmojiChangedEvent),
     MemberJoinedChannel(SlackMemberJoinedChannelEvent),
@@ -213,13 +212,6 @@ pub struct SlackMessageEventEdited {
 #[skip_serializing_none]
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize, Builder)]
 pub struct SlackAppUninstalledEvent {}
-
-#[skip_serializing_none]
-#[derive(Debug, PartialEq, Clone, Serialize, Deserialize, Builder)]
-pub struct SlackAssistantThreadStartedEvent {
-    pub assistant_thread: SlackAssistantThread,
-    pub event_ts: SlackTs,
-}
 
 #[skip_serializing_none]
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize, Builder)]
